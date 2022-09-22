27bb32ba72f8fcfdcd3116ef80ddb651fb52f54d
Joseph <joffan98@gmail.com>
2022-09-21 19:45:34 -0500

    commit tree 36f14e97b2e571e66ced04a5ddde13880946ddfc

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {name:"Daro", streetAddress:"Not happening"}
+
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+    const employeeCopy = {...employee}
+    employeeCopy[key] = value
+    return employeeCopy
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+    employee[key] = value
+    return employee
+}  
+
+const deleteFromEmployeeByKey = (employee, key) => {
+    const employeeCopy = {...employee}
+    delete employeeCopy[key]
+    return employeeCopy
+}
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+    delete employee[key]
+    return employee
+}
+
+console.log(deleteFromEmployeeByKey(employee, "name"))
